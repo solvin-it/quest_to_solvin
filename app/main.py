31aab76be978aac52787306645c4c027aed5b25c
Solvin IT <josefernando.a.gonzales@gmail.com>
2023-11-30 18:00:20 +0800

    Updated the folder structure of the project.

---
+++ app/main.py
@@ -0,0 +1,29 @@
+# Load the background story from the file
+
+# Print the background story to the screen
+
+# Display a generated image of the beginner village
+
+# Ask the user for their name, age, class, and weapon
+
+# Ask the user where they want to go based on the choices
+
+# Display a generated image of the location
+
+# Describe the location to the user
+
+# Ask the user who they want to talk to based on the choices
+
+# Generate NPC information such as name, age, profession, personality, and image
+
+# Display the NPC image to the user
+
+# Start a conversation with the NPC
+
+# Once the NPC learns enough from the user or the user asks for a quest, generate a quest
+
+# Display the quest to the user
+
+# Ask the user if they want to accept the quest
+
+# Continue the conversation with the NPC after the user accepts or rejects the quest and end the conversation
