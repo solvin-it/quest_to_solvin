e138839030800d8510ba876c59b3d4c7cf7fd9cc
Solvin IT <josefernando.a.gonzales@gmail.com>
2023-12-16 11:27:47 +0800

    Rearranged the code for readability. Changed the introduction. Displayed the village first before the intro. Added a condition to detect if it is time to generate a quest.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+from pathlib import Path
 import streamlit as st
 from world import World
 from npc import NPC
 import time
 from openai import OpenAI
+
+# Path settings
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "static" / "css" / "main.css"
 
 def simulate_typing(text):
     message_placeholder = st.empty()
@@ -14,75 +19,78 @@ def simulate_typing(text):
         message_placeholder.markdown(full_response + "▌")
     message_placeholder.markdown(full_response)
 
-def main(world):
+st.set_page_config(page_title="Quest To Solvin", page_icon=":video_game:",layout="wide")
 
-    # Set the sidebar
-    with st.sidebar:
-        # Set the title of the page
-        st.title("Chapter One: The Beginning")
+with open(css_file) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-        if "OPENAI_API_KEY" in st.secrets:
-            OpenAI.api_key = st.secrets["OPENAI_API_KEY"]
+# Set the sidebar
+with st.sidebar:
+    # Set the title of the page
+    st.title("Chapter One: The Beginning")
+
+    if "OPENAI_API_KEY" in st.secrets:
+        OpenAI.api_key = st.secrets["OPENAI_API_KEY"]
+        st.success("OpenAI API Key found!", icon="🔑")
+    else:
+        OpenAI.api_key = st.text_input("OpenAI API Key", type="password")
+        if (not OpenAI.api_key.startswith('sk-') or len (OpenAI.api_key ) != 51) and OpenAI.api_key != "":
+            st.error("Invalid API Key", icon="🚫")
+        elif OpenAI.api_key.startswith('sk-') and len (OpenAI.api_key ) == 51:
             st.success("OpenAI API Key found!", icon="🔑")
-        else:
-            OpenAI.api_key = st.text_input("OpenAI API Key", type="password")
-            if (not OpenAI.api_key.startswith('sk-') or len (OpenAI.api_key ) != 51) and OpenAI.api_key != "":
-                st.error("Invalid API Key", icon="🚫")
-            elif OpenAI.api_key.startswith('sk-') and len (OpenAI.api_key ) == 51:
-                st.success("OpenAI API Key found!", icon="🔑")
 
-        if 'npc' in st.session_state:
-            st.markdown("### Character Window:")
-            st.image(st.session_state.npc.image, caption=st.session_state.npc.name)
-            st.markdown(f"Name: {st.session_state.npc.name}")
-            st.markdown(f"Age: {st.session_state.npc.age}")
-            st.markdown(f"Profession: {st.session_state.npc.profession}")
-            st.markdown(f"Personality: {st.session_state.npc.personality}")
-            st.markdown(f"Description: {st.session_state.npc.description}")
+    if 'npc' in st.session_state:
+        st.markdown("### Character Window:")
+        st.image(st.session_state.npc.image, caption=st.session_state.npc.name)
+        st.markdown(f"Name: {st.session_state.npc.name}")
+        st.markdown(f"Age: {st.session_state.npc.age}")
+        st.markdown(f"Profession: {st.session_state.npc.profession}")
+        st.markdown(f"Personality: {st.session_state.npc.personality}")
+        st.markdown(f"Description: {st.session_state.npc.description}")
 
-    if OpenAI.api_key.startswith('sk-') and len (OpenAI.api_key ) == 51:
-        # Initialize the world and start the introduction.
-        if 'world' not in st.session_state:
-            st.session_state.world = world
+if OpenAI.api_key.startswith('sk-') and len (OpenAI.api_key ) == 51:
+    # Initialize the world and start the introduction.
+    if 'world' not in st.session_state:
+        st.session_state.world = World()
 
-            # Display the image of the empire
-            # st.image(st.session_state.world.get_location_image("solvin_empire"), caption="The Empire of Solvin")
+    if 'start' not in st.session_state:
+        st.session_state.start = False   
+
+    if 'quest' not in st.session_state:
+        st.session_state.quest = ""
+        st.session_state.quest_created = False
+
+    # Display the quest to the user
+    if not st.session_state.quest_created:
+        if not st.session_state.start:
+            # Display a generated image of the beginner village
+            st.image(st.session_state.world.get_location_image("eldertree"), caption="The Eldertree Village")
             
             # Print the background story to the screen
             for line in st.session_state.world.background:
-                message_placeholder = st.empty()
-                message_placeholder.markdown(line)
-
-            # Display a generated image of the beginner village
-            st.image(st.session_state.world.get_location_image("eldertree"), caption="The Eldertree Village")
-
-        if 'start' not in st.session_state:
-            st.session_state.start = False   
-
-        if 'quest' not in st.session_state:
-            st.session_state.quest = ""     
-
-        if st.session_state.start:
-            # Initialize  chat history
+                st.markdown(line)
+            
+            if st.button("Start"):
+                st.session_state.start = True
+                # Initialize the NPC
+                if "npc" not in st.session_state:
+                    st.session_state.npc = NPC(
+                        settings=st.session_state.world.settings
+                    )
+                st.rerun()
+        else:
+            # Initialize chat history
             if "messages" not in st.session_state:
                 st.session_state.messages = []
+
+                # Introduce the user to the village
+                st.markdown("As you step foot into the village of Eldertree, you encountered with one of its villagers.")
+                st.markdown("What do you say to the villager?")
 
             # Display the chat history
             for message in st.session_state.messages:
                 with st.chat_message(message['role']):
                     st.markdown(message['content'])
-
-            # Start a conversation with the NPC
-            if st.session_state.messages == []:
-                # Display assistant response in chat message container
-                with st.chat_message("assistant"):
-                    message_placeholder = st.empty()
-                    task = "Welcome the user to the Eldertree village."
-                    assistant_response = st.session_state.npc.generate_response([{"role": "assistant", "content": task}])
-                    
-                    simulate_typing(assistant_response)
-                # Add assistant response to chat history
-                st.session_state.messages.append({"role": "assistant", "content": assistant_response})
         
             if prompt := st.chat_input("Say something..."):
                 with st.chat_message("user"):
@@ -94,29 +102,15 @@ def main(world):
                     message_placeholder = st.empty()
                     messages = st.session_state.messages.copy()
                     assistant_response = st.session_state.npc.generate_response(messages)
+
+                    if "CREATE QUEST" in assistant_response:
+                        st.session_state.quest_created = True
+                        st.rerun()
                     
                     # Simulate stream of response with milliseconds delay
                     simulate_typing(assistant_response)
 
                 # Add assistant response to chat history
                 st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-
-                # Once the NPC learns enough from the user or the user asks for a quest, generate a quest
-
-                # Display the quest to the user
-
-                # Ask the user if they want to accept the quest
-
-                # Continue the conversation with the NPC after the user accepts or rejects the quest and end the conversation
-        else:
-            if st.button("Start"):
-                st.session_state.start = True
-                # Initialize the NPC
-                if "npc" not in st.session_state:
-                    st.session_state.npc = NPC(
-                        settings=st.session_state.world.settings
-                    )
-                st.rerun()
-
-if __name__ == "__main__":
-    main(World())
+    else:
+        st.header("Quest")
